def179eded4aa5d2a1d6282a0b2758f6fe125989
Habeeb <habeebahmadu1@gmail.com>
2024-10-18 23:03:59 +0100

    styles: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: '#FEFCFB',
+        dark: '#0A1128',
+        btn: '#FFA630',
+        icon: '#72E1D1',
+        modal: '#686963',
+      },
+    },
   },
   plugins: [],
 };
